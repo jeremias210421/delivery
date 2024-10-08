9110aa2effcddcb6950c3ca0b6f484412eb9549c
jeremias210421 <jeremias210421@gmail.com>
2024-10-08 16:54:33 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import HomePage from './HomePage';
+import './App.css'; // Não esqueça de importar seu CSS
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
